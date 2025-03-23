dba6ae9773542e31f958bb2cadd1be4bd376ddb5
leewan <lee.wan1204@gmail.com>
2025-03-23 20:00:02 +0000

    Feat: Added V2 ArticleService

--- BackendDotNet/FF.Articles.Backend/FF.Articles.Backend.Contents.API/Interfaces/Repositories/V2/IArticleRedisRepository.cs
+++ BackendDotNet/FF.Articles.Backend/FF.Articles.Backend.Contents.API/Interfaces/Repositories/V2/IArticleRedisRepository.cs
@@ -1,9 +1,13 @@
 ﻿using FF.Articles.Backend.Common.Bases.Interfaces;
 using FF.Articles.Backend.Contents.API.Models.Entities;
+using StackExchange.Redis;
 
 namespace FF.Articles.Backend.Contents.API.Interfaces.Repositories.V2
 {
     public interface IArticleRedisRepository : IRedisRepository<Article>
     {
+        Task<bool> UpdateAsync(Article newEntity, Article oldEntity);
+        Task<List<Article>> GetArticlesByTopicIdAsync(int topicId);
+        Task<List<Article>> GetChildArticlesAsync(int parentId);
     }
 }
